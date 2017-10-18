dccd86bb4039ce1ec9a007d7c3c4ea2837508170
baluragala <baluragala@gmail.com>
2017-10-18 17:49:20 +0530

    Signup and login implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import {Link} from 'react-router-dom';
 
@@ -6,8 +7,15 @@ const Home = (props) => (
   <div className="home">
     <p>Chocolate collection is a crowd sourced portal which gives you details about world renowned brands and
       chocolates</p>
+    {!props.isLoggedIn &&
     <p>Please <Link to="/signup">signup</Link>, to contribute. Happy chocolating :-)</p>
-  </div>
-);
+    }
+  </div>);
+
+Home.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string
+}
 
 export default Home
