5a8fe94993cb882b6a0a70405e4dfb901fc3be50
Balakrishna Ragala <baluragala@gmail.com>
2017-11-11 06:43:20 +0530

    dev tools and concurrently added

--- src/createStore.js
+++ src/createStore.js
@@ -1,17 +1,20 @@
-import {createStore, applyMiddleware} from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
-import {createLogger} from 'redux-logger'
+import { createLogger } from 'redux-logger'
 import rootSaga from './sagas'
 import createSagaMiddleware from 'redux-saga'
-import {Map} from 'immutable'
+import { Map } from 'immutable'
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 
 let initialState = {
-  brandsState: Map({brands: [], isFetching: true}),
-  chocolatesState: Map({chocolates: [], isFetching: true}),
-  usersState: Map({isLoggedIn: false, firstName: '', lastName: ''})
+  brandsState: Map({ brands: [], isFetching: true }),
+  chocolatesState: Map({ chocolates: [], isFetching: true }),
+  usersState: Map({ isLoggedIn: false, firstName: '', lastName: '' })
 };
 let sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware, createLogger()));
+const store = createStore(rootReducer, initialState, composeWithDevTools(
+  applyMiddleware(sagaMiddleware, createLogger())));
 sagaMiddleware.run(rootSaga);
 
 export default store;
